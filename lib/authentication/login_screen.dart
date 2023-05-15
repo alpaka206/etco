a80eb62f0395d13a3060d5c3e8501b2d576c5b30
popeye0618 <popeye0618@gmail.com>
2023-05-15 16:48:59 +0900

    0515

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -5,6 +5,12 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
