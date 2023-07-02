92b864c040424202d546d1cbd09ba3466abe7ade
popeye0618 <popeye0618@gmail.com>
2023-07-02 11:40:16 +0900

    0702

--- lib/authentication/find_idpw_screen.dart
+++ lib/authentication/find_idpw_screen.dart
@@ -200,10 +200,19 @@ class _FindIdPwScreenState extends State<FindIdPwScreen> {
                                 top: Sizes.size80,
                               ),
                               child: Container(
+                                width: 350,
                                 height: 180,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(10),
                                   color: const Color(0xfff3f3f3),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 5,
+                                      blurRadius: 7,
+                                      offset: const Offset(0, 3),
+                                    )
+                                  ],
                                 ),
                                 child: Center(
                                   child: Column(
