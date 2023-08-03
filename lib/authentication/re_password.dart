a6c2d654f0ce1cad793c33d750a0d9abe3041346
popeye0618 <popeye0618@gmail.com>
2023-08-03 18:53:10 +0900

    0803

--- lib/authentication/re_password.dart
+++ lib/authentication/re_password.dart
@@ -1,10 +1,196 @@
+import 'package:etco/authentication/re_password2.dart';
+import 'package:etco/authentication/widgets/auth_button.dart';
+import 'package:etco/constants/gaps.dart';
+import 'package:etco/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
-class RePasswordScreen extends StatelessWidget {
+class RePasswordScreen extends StatefulWidget {
   const RePasswordScreen({super.key});
 
   @override
+  State<RePasswordScreen> createState() => _RePasswordScreenState();
+}
+
+class _RePasswordScreenState extends State<RePasswordScreen> {
+  bool _isEmailChecked = false;
+  bool _isPhoneChecked = false;
+
+  void _onEmailTap() {
+    _isEmailChecked = !_isEmailChecked;
+    if (_isEmailChecked && _isPhoneChecked) _isPhoneChecked = false;
+    setState(() {});
+  }
+
+  void _onPhoneTap() {
+    _isPhoneChecked = !_isPhoneChecked;
+    if (_isEmailChecked && _isPhoneChecked) _isEmailChecked = false;
+    setState(() {});
+  }
+
+  void _onNextTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const RePasswordTwo(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size32,
+          ),
+          child: Column(
+            children: [
+              Gaps.v96,
+              const Text(
+                "ID / PW 찾기",
+                style: TextStyle(
+                  fontSize: Sizes.size44,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v72,
+              const Text(
+                "비밀번호 재설정",
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Gaps.v64,
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: "아이디",
+                  contentPadding: EdgeInsets.only(
+                    left: Sizes.size10,
+                    top: Sizes.size10,
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v1,
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: "이름",
+                  contentPadding: EdgeInsets.only(
+                    left: Sizes.size10,
+                    top: Sizes.size10,
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+              Gaps.v60,
+              Container(
+                height: Sizes.size96,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Sizes.size20,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "비밀번호 찾기 방법을\n선택해주세요",
+                      ),
+                      Gaps.h60,
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: _onEmailTap,
+                                  child: Container(
+                                    height: 15,
+                                    width: 15,
+                                    decoration: BoxDecoration(
+                                      color: _isEmailChecked
+                                          ? Theme.of(context).primaryColor
+                                          : Colors.transparent,
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Gaps.h10,
+                                const Text(
+                                  "E-mail 주소",
+                                ),
+                              ],
+                            ),
+                            Gaps.v6,
+                            Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: _onPhoneTap,
+                                  child: Container(
+                                    height: 15,
+                                    width: 15,
+                                    decoration: BoxDecoration(
+                                      color: _isPhoneChecked
+                                          ? Theme.of(context).primaryColor
+                                          : Colors.transparent,
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Gaps.h10,
+                                const Text(
+                                  "등록된 휴대폰 번호",
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Gaps.v20,
+              const Text(
+                "*입력하신 정보와 일치하는 계정이 없습니다",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Gaps.v32,
+              GestureDetector(
+                onTap: _onNextTap,
+                child: const AuthButton(
+                  text: "다음 >",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/authentication/re_password2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RePasswordTwo extends StatelessWidget {
+  const RePasswordTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
